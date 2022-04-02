ebb5c3f675957a185c358c7e78417122c0834bf5
Troy Neubauer <troyneubauer@gmail.com>
2022-04-02 07:09:03 -0400

    flash chip cannot be written to. Program failure=1

--- basic_example/src/main.rs
+++ basic_example/src/main.rs
@@ -5,6 +5,7 @@ use core::cell::UnsafeCell;
 use core::fmt::Write;
 use core::mem::MaybeUninit;
 
+use embedded_hal::digital::v2::OutputPin;
 use embedded_hal::spi::{Mode, Phase, Polarity};
 use hal::pac::USART2;
 
@@ -116,30 +117,135 @@ fn main() -> ! {
         })
         .unwrap();
 
+    flash.reset(&mut delay).unwrap();
+
+    let mut config = flash
+        .read_regester(Addresses::CONFIGURATION_REGISTER)
+        .unwrap();
+    config |= 1 << 4; // Enable ECC
+    config |= 1; // disable HOLD
+    flash
+        .write_register(Addresses::CONFIGURATION_REGISTER, config)
+        .unwrap();
+
+    fn status<SPI, CS, SE, PE>(chip: &mut W25n512gv<SPI, CS>)
+    where
+        SPI: embedded_hal::blocking::spi::Transfer<u8, Error = SE>
+            + embedded_hal::blocking::spi::Write<u8, Error = SE>,
+        CS: OutputPin<Error = PE>,
+    {
+        let val = chip
+            .read_regester(Addresses::STATUS_REGISTER)
+            .unwrap_or_else(|_| panic!());
+        let strs = [
+            "BUSY",
+            "Write enable",
+            "Erase Failure",
+            "Program failure",
+            "ECC 0",
+            "ECC 1",
+            "Look up table full",
+        ];
+        println!("Status:");
+        for (i, s) in strs.iter().enumerate() {
+            let bit = (val >> i) & 0b1;
+            if bit != 0 {
+                println!("{}: {}", s, bit);
+            }
+        }
+        println!();
+    }
+
+    fn protection<SPI, CS, SE, PE>(chip: &mut W25n512gv<SPI, CS>)
+    where
+        SPI: embedded_hal::blocking::spi::Transfer<u8, Error = SE>
+            + embedded_hal::blocking::spi::Write<u8, Error = SE>,
+        CS: OutputPin<Error = PE>,
+    {
+        let val = chip
+            .read_regester(Addresses::STATUS_REGISTER)
+            .unwrap_or_else(|_| panic!());
+        let strs = ["SRP1", "WP-E", "TB", "BP0", "BP1", "BP2", "BP3", "SRP0"];
+        println!("protection:");
+        for (i, s) in strs.iter().enumerate() {
+            let bit = (val >> i) & 0b1;
+            if bit != 0 {
+                println!("{}: {}", s, bit);
+            }
+        }
+        println!();
+    }
     println!("Initialized.");
+    protection(&mut flash);
     let id = flash.read_jedec_id().unwrap();
     println!("Id {id:?}");
 
+    status(&mut flash);
+
+    println!("Erasing chip...");
+    flash.enable_write().unwrap();
+    flash.chip_erase().unwrap();
+    println!("Done!");
+
+    fn dump_flash<SPI, CS, SE, PE>(chip: &mut W25n512gv<SPI, CS>)
+    where
+        SPI: embedded_hal::blocking::spi::Transfer<u8, Error = SE>
+            + embedded_hal::blocking::spi::Write<u8, Error = SE>,
+        CS: OutputPin<Error = PE>,
     {
-        let status = flash.status_register().unwrap();
-        println!("before {:0b}", status.reg.get());
+        let mut buf = [0u8; w25n512gv::RAW_PAGE_SIZE];
+        chip.page_read_continous(&mut buf)
+            .unwrap_or_else(|_| panic!());
+
+        for i in 0..16 {
+            print!("{}, ", buf[i]);
+        }
+        println!();
     }
 
-    flash.disable_write().unwrap();
-    {
-        let status = flash.status_register().unwrap();
-        println!("after {:0b}", status.reg.get());
-        status.reg.WriteEnableLatch().set(true)
+    println!();
+    println!("before");
+    let test_page = 2u16.pow(6);
+    flash.page_data_read(test_page).unwrap();
+    dump_flash(&mut flash);
+    status(&mut flash);
+
+    let mut index: u8 = 0;
+    let test_data = [0u8; w25n512gv::PAGE_SIZE_WITH_ECC].map(|_| {
+        index = index.wrapping_add(1);
+        index
+    });
+    delay.delay_us(10u8);
+
+    flash.enable_write().unwrap();
+    flash.load_program_data(0, &test_data).unwrap();
+    for i in 0..128 {
+        println!("in buffer");
+        status(&mut flash);
+        dump_flash(&mut flash);
+        delay.delay_us(10u8);
+        flash.enable_write().unwrap();
+        flash.program_execute(i).unwrap();
     }
 
-    {
-        let status = flash.status_register().unwrap();
-        println!("after after {:0b}", status.reg.get());
-    }
+    println!("after");
+    status(&mut flash);
+    flash.page_data_read(test_page).unwrap();
+    dump_flash(&mut flash);
 
-    //println!("config {:0b}", flash.read_regester(Addresses::CONFIGURATION_REGISTER).unwrap());
-    //println!("protec {:0b}", flash.read_regester(Addresses::PROTECTION_REGISTER).unwrap());
+    status(&mut flash);
+    println!(
+        "config {:0b}",
+        flash
+            .read_regester(Addresses::CONFIGURATION_REGISTER)
+            .unwrap()
+    );
+    println!(
+        "protec {:0b}",
+        flash.read_regester(Addresses::PROTECTION_REGISTER).unwrap()
+    );
 
+    println!("OK");
     loop {
         /*let sample = ms6511
             .get_second_order_sample(Oversampling::OS_256, &mut delay)
